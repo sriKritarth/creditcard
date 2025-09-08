db010a75196f2de10de35ab5f28a1100f1a5829b
Kritarth Srivastava <oftheleaf.hidden@gmail.com>
2025-09-08 21:23:29 +0530

    Changed data & model versioning for optimization

--- track.py
+++ track.py
@@ -9,11 +9,15 @@ with mlflow.start_run():
     mlflow.log_param("epochs", epochs)
     for epoch in range(epochs):
         train_acc = epoch + random.random()
-        train_loss = epochs - epoch - random.random
+        train_loss = epochs - epoch - random.random()
         val_acc = epoch + random.random()
-        val_loss = epochs - epoch - random.random
+        val_loss = epochs - epoch - random.random()
 
         mlflow.log_metric("train/accuracy", train_acc)
         mlflow.log_metric("train/loss", train_loss)
         mlflow.log_metric("val/accuracy", val_acc)
         mlflow.log_metric("val/loss", val_loss)
+
+with open("metrics.txt" , 'w') as outfile:
+    outfile.write("Final val accuracy: " + str(val_acc) + "\n")
+    outfile.write("Final loss accuracy: " + str(val_loss) + "\n")
